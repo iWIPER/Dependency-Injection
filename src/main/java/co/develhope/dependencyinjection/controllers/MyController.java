c3b9edbd18d0079bb99466b88c87940eab06b3db
Wilk Pereira <hhgchdbfgf@gmail.com>
2022-09-09 14:02:05 +0200

    Exercise Dependency Injection

---
+++ src/main/java/co/develhope/dependencyinjection/controllers/MyController.java
@@ -0,0 +1,29 @@
+package co.develhope.dependencyinjection.controllers;
+
+import co.develhope.dependencyinjection.services.MyService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MyController {
+
+    private MyService service;
+
+    public MyController(MyService service) {
+        System.out.println("MyController constructor has been called");
+        this.service = service;
+    }
+
+    @GetMapping
+    public String helloMsg(){
+        return "Hello, thanks for visiting us!";
+    }
+
+    @GetMapping("/getName")
+    public String getName(){
+        return service.getName();
+    }
+
+}
